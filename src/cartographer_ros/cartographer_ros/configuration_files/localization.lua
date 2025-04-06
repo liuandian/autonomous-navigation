3528c4e0aba94a55460714dc96780f2334428ba4
GoFuuu <1279063580@qq.com>
2025-04-07 06:14:11 +0800

    first commit

--- src/cartographer_ros/cartographer_ros/configuration_files/localization.lua
+++
@@ -1,9 +0,0 @@
-include "2d_online.lua"
-
-TRAJECTORY_BUILDER.pure_localization_trimmer = {
-    max_submaps_to_keep = 3,
-}
-MAP_BUILDER.num_background_threads = 8
-POSE_GRAPH.optimize_every_n_nodes = 80
-
-return options
